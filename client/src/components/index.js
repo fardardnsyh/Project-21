ab5bee5c51d63918334f9815cd756afdcb4a8115
Farid Ardiansyah <faridardiansyah061@gmail.com>
2024-08-15 07:29:36 +0700

    Files upload

---
+++ client/src/components/index.js
@@ -0,0 +1,19 @@
+import Logo from "./Logo";
+import FormRow from "./FormRow";
+import Alert from "./Alert";
+import Navbar from "./Navbar";
+import SmallSidebar from "./SmallSidebar";
+import BigSidebar from "./BigSidebar";
+import SearchContainer from "./SearchContainer";
+import JobsContainer from "./JobsContainer";
+
+export {
+  Logo,
+  FormRow,
+  Alert,
+  Navbar,
+  SmallSidebar,
+  BigSidebar,
+  SearchContainer,
+  JobsContainer,
+};
